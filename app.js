cea9191b15a20e7f511ba077a70680ac5a20b1c5
Mridul2820 <immridulpanda2820@gmail.com>
2021-03-31 20:50:43 +0530

    yelpcamp v5

--- app.js
+++ app.js
@@ -6,13 +6,11 @@ var Campground = require("./models/campground");
 var seedDB = require("./seeds");
 var Comment = require("./models/comment");
 
-seedDB();
-
 mongoose.connect("mongodb://localhost/yelp_camp", { useNewUrlParser: true });
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs")
-
+app.use(express.static(__dirname + "/public"))
+seedDB();
 
 
 // Campground.create(
